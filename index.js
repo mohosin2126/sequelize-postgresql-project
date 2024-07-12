15c50d4de2f2f82766420b8d10112417a05fe93c
mohosin2126 <mohosin2126@gmail.com>
2024-07-12 21:22:44 +0600

    imported dotenv file

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// require the express
 const express = require('express');
-const authRouter=require("./route/authRoute")
 const app=express()
-const port = 3000;
+
+// import the route
+const authRouter=require("./route/authRoute")
+
+// import the dotenv
+require("dotenv").config({path:`${process.cwd()}/.env`})
+
 
 app.get("/",(req,res)=>{
  res.send("Welcome to the server")
 })
 
-app.use("/api/v1/auth",auth)
+// app.use("/api/v1/auth",auth)
 
 
-app.listen(port,()=>{
-    console.log("Server listening on port "+port)
+app.listen(process.env.APP_PORT,()=>{
+    console.log("Server listening on port "+process.env.APP_PORT)
 })
